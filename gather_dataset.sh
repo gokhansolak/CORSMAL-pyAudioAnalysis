5da4b1ec9d5555e9fe1c47468b0bfb5f8c44e036
Gokhan Solak <gknsolak@gmail.com>
2020-09-02 11:54:33 +0100

    3-fold split & classify

--- gather_dataset.sh
+++ gather_dataset.sh
@@ -2,20 +2,20 @@
 # Moves CORSMAL audio files into class folders; splits train and test folders.
 # NOTE: it will overwrite the files in the target folder, if any.
 
-move_case(){
+function move_case(){
   i=$1
+  case_path=$2
 
-  echo -e "\nobject $i"
+  echo "object $i"
   cd "${source_path}/$i/audio"
 
   # iterate classes
   for j in {0,1,2,3}; do
-    echo -e "\n class $j"
+    echo " class $j"
 
     file_prefix="s[0-9]_fi${j}_fu[0-9]_b[0-9]_l[0-9]_audio\.wav"
 
-    mkdir -p "${target_path}/fi${j}"
-    mkdir -p "${target_path}/test/fi${j}"
+    mkdir -p "${case_path}/fi${j}"
 
     set_i=0
 
@@ -24,23 +24,40 @@ move_case(){
       # echo $f
       if [[ "$f" =~ ^${file_prefix} ]]; then
 
-        if [[ "$set_i" -eq 4 ]]; then
-          # test set
-          let "set_i=0"
-          # compressing audio because the library wants mono audio
-          ffmpeg -y -v 0 -i "${BASH_REMATCH[0]}" -ac 1 "${target_path}/test/fi${j}/o${i}_${BASH_REMATCH[0]}"
-        else
-          # training set
-          ffmpeg -y -v 0 -i "${BASH_REMATCH[0]}" -ac 1 "${target_path}/fi${j}/o${i}_${BASH_REMATCH[0]}"
-        fi
-        let "set_i++"
+        # compressing audio because the library wants mono audio
+        ffmpeg -y -v 0 -i "${BASH_REMATCH[0]}" -ac 1 "${case_path}/fi${j}/o${i}_${BASH_REMATCH[0]}"
 
         ## progress dots
-        echo -n "."
+        # echo -n "."
 
       fi
 
     done
+
+    # echo "*"
+
+  done
+
+}
+
+function create_fold(){
+
+  fold_no=$1
+
+  for i in {1,2,3,4,5,6,7,8,9}; do
+
+    # modulus for object types (cup,glass,box)
+    mod_i=$(($i % 3))
+
+    if [[ "$mod_i" -eq ${fold_no} ]]; then
+      # test set
+      echo "test"
+      move_case "$i" "${target_path}/test${fold_no}"
+    else
+      # train set
+      echo "train"
+      move_case "$i" "${target_path}/train${fold_no}"
+    fi
 
   done
 
@@ -56,10 +73,10 @@ initial_path="$PWD"
 source_path="$PWD/$1"
 target_path="$PWD/$2"
 
-
-for i in {1,2,3,4,5,6,7,8,9}; do
-
-  move_case "$i"
+# create 3-folds
+for fold in {0,1,2}; do
+  echo "=== fold $fold"
+  create_fold ${fold}
 
 done
 
