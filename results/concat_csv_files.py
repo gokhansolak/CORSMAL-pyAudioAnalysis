e17d38520df3c8c7d868841c09225d941f02104f
Gokhan Solak <gknsolak@gmail.com>
2020-09-03 22:30:55 +0100

    Combined outputs of vggish & pyaudio
    Improves for ftype but not for flevel

---
+++ results/concat_csv_files.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+if __name__ == '__main__':
+
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument('-f', '--files', nargs='+', help='List of space separated file paths.', required=True)
+    parser.add_argument('-o', '--output', help='Name of the output file, w/o extension.', default='combined')
+
+    args = parser.parse_args()
+
+    df_list = []
+    for f in args.files:
+        df_list.append(pd.read_csv(f))
+
+    df_comb = pd.concat(df_list)
+
+    df_comb.to_csv(args.output+'.csv', index=False)
